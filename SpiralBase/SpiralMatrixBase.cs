99234ee9fb648b385d9555cfe7547cfd93636a82
AlexTykoon <alexandertykun@coherentsolutions.com>
2023-06-16 19:38:43 +0300

    First commit

---
+++ SpiralBase/SpiralMatrixBase.cs
@@ -0,0 +1,28 @@
+﻿namespace SpiralBase;
+
+public abstract class SpiralMatrixBase
+{
+    private readonly int _rows;
+    private readonly int _cols;
+
+    public int Rows => _rows;
+    public int Cols => _cols;
+    public int MaxElement => _rows * _cols;
+
+    public abstract int this[int i, int j] { get; }
+    public abstract (int i, int j) GetIndices(int k);
+
+    public SpiralMatrixBase(int rows, int cols)
+    {
+        _rows = rows;
+        _cols = cols;
+    }
+
+    protected void CheckIndices(int i, int j)
+    {
+        if (i < 0 || j < 0 || i >= _rows || j >= _cols)
+        {
+            throw new ArgumentOutOfRangeException("wrong indices");
+        }
+    }
+}
---
+++ SpiralGreedy/SpiralMatrixG.cs
@@ -0,0 +1,20 @@
+﻿namespace SpiralGreedy;
+
+public class SpiralMatrixG : SpiralBase.SpiralMatrixBase
+{
+    public SpiralMatrixG(int rows, int columns) : base(rows, columns) { }
+
+    public override int this[int i, int j]
+    {
+        get
+        {
+            CheckIndices(i, j);
+            throw new NotImplementedException();
+        }
+    }
+
+    public override (int i, int j) GetIndices(int k)
+    {
+        throw new NotImplementedException();
+    }
+}
